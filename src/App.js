978a90ec8e79f32404a383458995f6e9a5a0af8f
JavierAcosta <jaacosta7433@misena.edu.co>
2020-03-28 17:43:12 -0500

    [javieracosta:#master:refactory]se crean el proyecto en react de un crud sencillo sin base de datos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import UserTable from './components/User.Table'
+import AddUserForm from './components/AddUserForm'
+import EditUserForm from './components/EditUserForm'
+import { v4 as uuidv4 } from 'uuid';
+  const App = () => {
 
-function App() {
+    const usersData = [
+      { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+      { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+      { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+    ];
+
+  // state
+  const [users, setUsers] =useState(usersData);
+
+  // agregar
+
+  const addUser = (user) =>{
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+    
+  }
+
+  // eliminar Usuarios
+
+  const deleteUser = (id) => {
+
+    setUsers(users.filter(user => user.id !== id))
+
+  }
+
+  // editar
+
+const [editing, setEditing] = useState(false);
+const [currentUser, setCurrenUser] = useState({id: null, name:'',username:''});
+
+const editRow = (user) => {
+  setEditing(true);
+  setCurrenUser({
+    id:user.id , name:user.name, username:user.username
+  })
+}
+
+const updateUser = (id, updateUser) =>{
+  setEditing(false);
+
+  setUsers(users.map(user => (user.id === id ? updateUser: user)))
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="flex-row">
+        <div className="flex-large">
+
+        {
+          editing ?(
+            <div>
+              <h2>Edit user</h2>
+          <EditUserForm
+          currentUser={currentUser} 
+          updateUser={updateUser}
+          />
+            </div>
+          ) : (
+            <div>
+            <h2>Add user</h2>
+            <AddUserForm addUser={addUser} />
+            </div>
+          )
+
+        }
+    
+        </div>
+        <div className="flex-large">
+          <UserTable users={users}
+           deleteUser={deleteUser}
+           editRow={editRow}/>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
