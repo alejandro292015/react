2a4f007db8f42ad83b331fadc2169e5b985a982b
alejandro acosta <jaacosta7433@misena.edu.co>
2020-05-24 10:58:27 -0500

    proyecto

--- src/App.js
+++ src/App.js
@@ -17,6 +17,86 @@ import Work from './components/workout';
 
     </div>
     )
+    const usersData = [
+      { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+      { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+      { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+    ];
+
+  // state
+  const [users, setUsers] =useState(usersData);
+
+  // agregar
+
+  const addUser = (user) =>{
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+    
+  }
+
+  // eliminar Usuarios
+
+  const deleteUser = (id) => {
+
+    setUsers(users.filter(user => user.id !== id))
+
+  }
+
+  // editar
+
+const [editing, setEditing] = useState(false);
+const [currentUser, setCurrenUser] = useState({id: null, name:'',username:''});
+
+const editRow = (user) => {
+  setEditing(true);
+  setCurrenUser({
+    id:user.id , name:user.name, username:user.username
+  })
+}
+
+const updateUser = (id, updateUser) =>{
+  setEditing(false);
+
+  setUsers(users.map(user => (user.id === id ? updateUser: user)))
+}
+
+
+  return (
+    <div className="container">
+      <div className="flex-row">
+        <div className="flex-large">
+
+        {
+          editing ?(
+            <div>
+              <h2>Edit user</h2>
+          <EditUserForm
+          currentUser={currentUser} 
+          updateUser={updateUser}
+          />
+            </div>
+          ) : (
+            <div>
+            <h2>Add user</h2>
+            <AddUserForm addUser={addUser} />
+            </div>
+          )
+
+        }
+    
+        </div>
+        <div className="flex-large">
+          <UserTable users={users}
+           deleteUser={deleteUser}
+           editRow={editRow}/>
+        </div>
+      </div>
+    </div>
+  );
+
 
    
   }
